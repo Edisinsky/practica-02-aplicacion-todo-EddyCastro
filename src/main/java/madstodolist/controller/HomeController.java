adf6bbf0d5dfef7a60b254dd67951915d2cdd1b6
Eddy <eddy.castro@epn.edu.ec>
2025-06-07 15:53:59 -0500

    agregado método listaUsuarios para usarlo posteriormente con Thymeleaf

--- src/main/java/madstodolist/controller/HomeController.java
+++ src/main/java/madstodolist/controller/HomeController.java
@@ -1,18 +1,28 @@
 package madstodolist.controller;
 
+import madstodolist.model.Usuario;
+import madstodolist.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
+    @Autowired
+    UsuarioRepository usuarioRepository;
 
     @GetMapping("/about")
     public String about(Model model) {
         return "about";
     }
     @GetMapping("/registrados")
-    public String registrados(Model model) {
+
+    public String listaUsuarios(Model model) {
+        List<Usuario> usuarios = (List<Usuario>) usuarioRepository.findAll();
+        model.addAttribute("usuarios", usuarios);
         return "registrados";
     }
 
