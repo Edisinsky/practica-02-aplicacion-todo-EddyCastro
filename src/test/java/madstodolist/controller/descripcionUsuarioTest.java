1ea2bee6ec13d0432189f2799aec6885999b72b3
Eddy <eddy.castro@epn.edu.ec>
2025-06-10 13:20:49 -0500

    Creación de descripcionUsuarioTest con el método descripcionUsuariosDevuelveVistaYUsuarioEnModelo

---
+++ src/test/java/madstodolist/controller/descripcionUsuarioTest.java
@@ -0,0 +1,51 @@
+package madstodolist.controller;
+
+import madstodolist.model.Usuario;
+import madstodolist.repository.UsuarioRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+import org.springframework.ui.Model;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class descripcionUsuarioTest {
+
+    @Mock
+    UsuarioRepository usuarioRepository;
+
+    @Mock
+    Model model;
+
+    @InjectMocks
+    HomeController homeController;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void descripcionUsuariosDevuelveVistaYUsuarioEnModelo() {
+        // Arrange
+        Long id = 1L;
+        Usuario usuario = new Usuario("ejemplo@email.com");
+        usuario.setId(id);
+        usuario.setNombre("Usuario de prueba");
+
+        when(usuarioRepository.findById(id)).thenReturn(Optional.of(usuario));
+
+        // Act
+        String vista = homeController.descripcionUsuarios(id, model);
+
+        // Assert
+        verify(usuarioRepository).findById(id); // verifica que se haya llamado
+        verify(model).addAttribute("usuario", usuario); // verifica que se añade al modelo
+        assertEquals("descripcionUsuarios", vista); // nombre de la vista devuelta
+    }
+
+
+}
